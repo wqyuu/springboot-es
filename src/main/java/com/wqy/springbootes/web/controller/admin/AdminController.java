9388a880f16206e3550bdf74ca80a3ecb8cad481
wqyuu <2549067835@qq.com>
2019-09-23 17:01:03 +0800

    增加指定运行配置

---
+++ src/main/java/com/wqy/springbootes/web/controller/admin/AdminController.java
@@ -0,0 +1,121 @@
+package com.wqy.springbootes.web.controller.admin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+import com.wqy.springbootes.base.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+
+/**
+ * Created by 瓦力.
+ */
+@Controller
+public class AdminController {
+
+
+    @Autowired
+    private Gson gson;
+
+    /**
+     * 后台管理中心
+     * @return
+     */
+    @GetMapping("/admin/center")
+    public String adminCenterPage() {
+        return "admin/center";
+    }
+
+    /**
+     * 欢迎页
+     * @return
+     */
+    @GetMapping("/admin/welcome")
+    public String welcomePage() {
+        return "admin/welcome";
+    }
+
+    /**
+     * 管理员登录页
+     * @return
+     */
+    @GetMapping("/admin/login")
+    public String adminLoginPage() {
+        return "admin/login";
+    }
+
+    /**
+     * 房源列表页
+     * @return
+     */
+    @GetMapping("admin/house/list")
+    public String houseListPage() {
+        return "admin/house-list";
+    }
+
+    /**
+     * 新增房源功能页
+     * @return
+     */
+    @GetMapping("/admin/add/house")
+    public String addHousePage() {
+        return "admin/house-add";
+    }
+
+    /**
+     * 上传图片接口
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "/admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+
+        String fileName = file.getOriginalFilename();
+        File target = new File("D:/APP/SpingBoot/uploadFile_dir/springboot-es/"+fileName);
+        try {
+            file.transferTo(target);
+            InputStream inputStream = file.getInputStream();
+            /*Response response = qiNiuervice.uploadFile(inputStream);
+            if (response.isOK()) {
+                QiNiuPutRet ret = gson.fromJson(response.bodyString(), QiNiuPutRet.class);
+                return ApiResponse.ofSuccess(ret);
+            } else {S
+                return ApiResponse.ofMessage(response.statusCode, response.getInfo());
+            }*/
+            return ApiResponse.ofSuccess(null);
+        } catch (QiniuException e) {
+            Response response = e.response;
+            try {
+                return ApiResponse.ofMessage(response.statusCode, response.bodyString());
+            } catch (QiniuException e1) {
+                e1.printStackTrace();
+                return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (IOException e) {
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+}
+
